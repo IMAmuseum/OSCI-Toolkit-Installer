28853ce14d4f1c89734816a212a15afeb9db4f50
Jeremy Tubbs <jeremytubbs@gmail.com>
2015-06-29 13:18:54 -0400

    osci-build.sh

--- osci-build.sh
+++ osci-build.sh
@@ -2,5 +2,19 @@
 
 chmod -R 755 sites/default/files
 chmod -R 755 sites/default/OSCI-Toolkit-Frontend
-cd osci-assets
-bash move-dependencies.sh ..
+# symlink OSCI-Toolkit-Frontend into frontend
+mkdir -p frontend
+cd frontend
+ln -s ../sites/default/OSCI-Toolkit-Frontend
+cd ..
+# symlink OSCI-Toolkit-Frontend into libraries
+mkdir -p sites/all/libraries
+cd sites/all/libraries
+ln -s ../../default/OSCI-Toolkit-Frontend
+cd ../../..
+# symlink OSCI-Toolkit into modules
+if [ ! -d sites/default/modules ]; then
+    mkdir -p sites/default/modules
+fi
+cd sites/default/modules
+ln -s ../OSCI-Toolkit/modules/* .
